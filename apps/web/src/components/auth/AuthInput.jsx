0988ce2fc144cdbe3c9959509e02c1ce2fc48e06
Farshasepta <farsha08septa@gmail.com>
2026-03-11 16:27:29 +0000

    refactor: memindahkan tombol-tombol ke login/signup form

--- apps/web/src/components/auth/AuthInput.jsx
+++
@@ -1,8 +0,0 @@
-export default function AuthInput({ className = "", ...props }) {
-  return (
-    <input
-      className={`w-full border border-gray-300 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-sky-400 ${className}`}
-      {...props}
-    />
-  );
-}
